dd2ff2202cf21ce3d64f22f6d25e00c09241af94
Yan12001 <yan.beandapa@gmail.com>
2026-03-11 13:32:57 +0100

    change name app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'hello_world',
+  appName: 'My Kahoot',
   webDir: 'www'
 };
 
